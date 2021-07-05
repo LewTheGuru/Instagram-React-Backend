a9f63b7bd011248bedabbe53bd1b5132bc330817
LewTheDev <LewTheDev@gmail.com>
2021-07-05 15:36:14 -0700

    User Registration

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API is Live'));
 
 //  Routes
-
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
